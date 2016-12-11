0bdf80e32f5a83badd870f8df22b1728c6156776
William Davies <wd6g14@soton.ac.uk>
2016-12-11 16:13:14 +0000

    Added comments for ideas for third classifier

--- src/main/java/StEEl/run3/ComplexClassifier.java
+++ src/main/java/StEEl/run3/ComplexClassifier.java
@@ -1,11 +1,16 @@
 package StEEl.run3;
 
 import StEEl.AbstractClassifier;
+import org.openimaj.data.dataset.Dataset;
 import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.image.FImage;
-
+import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.util.pair.IntFloatPair;
 
 public class ComplexClassifier extends AbstractClassifier
 {
@@ -24,7 +29,12 @@ public class ComplexClassifier extends AbstractClassifier
 	@Override
 	public final void train(final GroupedDataset<String, ListDataset<FImage>, FImage> trainingSet)
 	{
+		//Dense sift pyramid
+		//Assigner assigning sift features to visual word, trainQuantiser
 
+		//Feature extractor based on bag of visual words
+
+		//Start training
 	}
 
 
@@ -37,6 +47,52 @@ public class ComplexClassifier extends AbstractClassifier
 	@Override
 	public final ClassificationResult<String> classify(final FImage object)
 	{
+		//naive bayes annotator classify
 		return null;
+	}
+
+	//Generate visual words
+	static HardAssigner<byte[], float[], IntFloatPair> trainQuantiser(Dataset<FImage> sample, PyramidDenseSIFT<FImage> pdsift) {
+
+		//List of sift features from training set
+
+		//For each image
+			//Get sift features
+
+		//Reduce set of sift features for time
+
+		//Create a kmeans classifier with 600 categories (600 visual words)
+
+		//Generate clusters (Visual words) from sift features.
+
+		return null;
+	}
+
+
+	//Extract bag of visual words feature vector
+	class PHOWExtractor implements FeatureExtractor<DoubleFV, FImage>
+	{
+		PyramidDenseSIFT<FImage> pdsift;
+		HardAssigner<byte[], float[], IntFloatPair> assigner;
+
+		public PHOWExtractor(PyramidDenseSIFT<FImage> pdsift, HardAssigner<byte[], float[], IntFloatPair> assigner)
+		{
+
+		}
+
+		public DoubleFV extractFeature(FImage image) {
+
+			//Get sift features of input image
+
+
+			//Bag of visual words histogram representation
+
+
+			//Bag of visual words for blocks and combine
+
+
+			//Return normalised feature vector
+			return null;
+		}
 	}
 }
